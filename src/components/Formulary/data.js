6bdc26e49ade8ed96da4a519768675b04bfe167b
alejandromorelosba <alejandromorelosba@gmail.com>
2026-07-18 22:32:13 -0500

    se ha actualizado

---
+++ src/components/Formulary/data.js
@@ -0,0 +1,117 @@
+/** se agrega el estilo css en el documento  **/
+import "./dataStyle.css"
+
+
+/** se realiza el questionario**/
+export const data = [
+    {
+    questionText: "¿Cuál es el gato más común en Colombia según la página?:",
+    name: "q1-name",
+    options: [
+        { choice: "a) Siamés", radioValue: "q1-a", selected: false },
+        { choice: "b) Persa", radioValue: "q1-b", selected: false },
+        {
+        choice: "c) Gato Criollo o Mestizo",
+        radioValue: "q1-c",
+        selected: false,
+        },
+        {
+        choice: "d) Bengalí",
+        radioValue: "q1-d",
+        selected: false,
+        },
+    ],
+    },
+    {
+    questionText: "¿Qué característica hace que el gato criollo sea muy resistente?",
+    name: "q2-name",
+    options: [
+        { choice: "a) Su pelaje largo.", radioValue: "q2-a", selected: false },
+        { choice: "b) Su diversidad genética.", radioValue: "q2-b", selected: false },
+        { choice: "c) Sus ojos azules", radioValue: "q2-c", selected: false },
+        { choice: "d) Su gran tamaño.", radioValue: "q2-d", selected: false },
+    ],
+    },
+    {
+    questionText:
+        "¿Qué raza se caracteriza por tener un pelaje largo y sedoso y una cara redonda y aplanada?",
+    name: "q3-name",
+    options: [
+        { choice: "a) Himalayo", radioValue: "q3-a", selected: false },
+        { choice: "b) Siamés", radioValue: "q3-b", selected: false },
+        { choice: "c) Persa", radioValue: "q3-c", selected: false },
+        { choice: "d) Ragdoll", radioValue: "q3-d", selected: false },
+    ],
+    },
+    {
+    questionText: "¿Qué raza es conocida por sus ojos azules profundos y por ser muy vocal?",
+    name: "q4-name",
+    options: [
+        { choice: "a) Maine Coon", radioValue: "q4-a", selected: false },
+        { choice: "b) Siamés", radioValue: "q4-b", selected: false },
+        { choice: "c) Azul Ruso", radioValue: "q4-c", selected: false},
+        { choice: "d) British Shorthair", radioValue: "q4-d", selected: false}
+    ],
+    },
+    {
+    questionText: "¿Qué raza tiene un pelaje con manchas que recuerdan a un tigrillo y necesita mucho espacio para jugar?",
+    name: "q5-name",
+    options: [
+        { choice: "a) Bengalí", radioValue: "q5-a", selected: false },
+        { choice: "b) Persa", radioValue: "q5-b", selected: false },
+        { choice: "c) Esfinge", radioValue: "q5-c", selected: false},
+        { choice: "d) Ragdoll", radioValue: "q5-d", selected: false}
+    ],
+    },
+        {
+    questionText: "¿Cuál es una de las razas de gatos más grandes, que puede llegar a pesar hasta 14 kg?",
+    name: "q6-name",
+    options: [
+        { choice: "a) British Shorthair", radioValue: "q6-a", selected: false },
+        { choice: "b) Persa", radioValue: "q6-b", selected: false },
+        { choice: "c) Maine Coon", radioValue: "q6-c", selected: false},
+        { choice: "d) Himalayo", radioValue: "q6-d", selected: false}
+    ],
+    },
+            {
+    questionText: "¿Qué raza prácticamente no tiene pelo y requiere cuidados especiales de la piel?",
+    name: "q7-name",
+    options: [
+        { choice: "a) Esfinge (Sphynx)", radioValue: "q7-a", selected: false },
+        { choice: "b) Azul Ruso", radioValue: "q7-b", selected: false },
+        { choice: "c) Siamés", radioValue: "q7-c", selected: false},
+        { choice: "d) Ragdoll", radioValue: "q7-d", selected: false}
+    ],
+    },
+            {
+    questionText: "¿Qué raza prefiere ambientes tranquilos y no le gustan los lugares ruidosos?",
+    name: "q8-name",
+    options: [
+        { choice: "a) Bengalí", radioValue: "q8-a", selected: false },
+        { choice: "b) Himalayo", radioValue: "q8-b", selected: false },
+        { choice: "c) Siamés", radioValue: "q8-c", selected: false},
+        { choice: "d) Maine Coon", radioValue: "q8-d", selected: false}
+    ],
+    },
+            {
+    questionText: "¿Qué raza posee un pelaje gris azulado y llamativos ojos verdes?",
+    name: "q9-name",
+    options: [
+        { choice: "a) British Shorthair", radioValue: "q9-a", selected: false },
+        { choice: "b) Persa", radioValue: "q9-b", selected: false },
+        { choice: "c) Azul Ruso", radioValue: "q9-c", selected: false},
+        { choice: "d) Ragdoll", radioValue: "q9-d", selected: false}
+    ],
+    },
+            {
+    questionText: "¿Qué raza es famosa por relajarse completamente cuando la cargan y es muy recomendada para familias?",
+    name: "q10-name",
+    options: [
+        { choice: "a) Ragdoll", radioValue: "q10-a", selected: false },
+        { choice: "b) Siamés", radioValue: "q10-b", selected: false },
+        { choice: "c) Bengalí", radioValue: "q10-c", selected: false},
+        { choice: "d) Esfinge (Sphynx)", radioValue: "q10-d", selected: false}
+    ],
+    },
+    
+];
